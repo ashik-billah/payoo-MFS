30fe133116cca87ccaeb07c6e78406db5889cfc1
ashik-billah <ashik.210101@s.pust.ac.bd>
2025-02-23 23:59:23 +0600

    added add money event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-console.log('home added')
+// addd money to the account
+
+
+//step-1 add event handler to the add money button inside the button
+
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    //prevent page reload after form submit
+    event.preventDefault();
+
+    
+    
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+
+
+});
